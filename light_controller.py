103337401bf6c7e6f5ff072386ab878fd2f9ec1f
mraveri <mcraveri1@yahoo.it>
2021-01-15 23:19:19 -0500

    Added stuff

--- light_controller.py
+++ light_controller.py
@@ -82,12 +82,12 @@ def main():
 
     # some initial fireworks:
     pixels.fill((0, 0, 0))
-    color_chase(RED, 0.05, pixels)
-    color_chase(YELLOW, 0.05, pixels)
-    color_chase(GREEN, 0.05, pixels)
-    color_chase(CYAN, 0.05, pixels)
-    color_chase(BLUE, 0.05, pixels)
-    color_chase(PURPLE, 0.05, pixels)
+    color_chase(RED, 0.0, pixels)
+    color_chase(YELLOW, 0.0, pixels)
+    color_chase(GREEN, 0.0, pixels)
+    color_chase(CYAN, 0.0, pixels)
+    color_chase(BLUE, 0.0, pixels)
+    color_chase(PURPLE, 0.0, pixels)
     rainbow_cycle(0, pixels)
     pixels.fill((0, 0, 0))
     pixels[0] = GREEN
